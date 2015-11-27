3343b4e80ed9e875f9b801fa1c6d5803ebbb3d54
josecarlosqs <josec.quichiz@gmail.com>
2015-11-27 16:09:04 -0500

    Fe de erratas ^^'

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 
 //Listener del socket para los eventos
 io.on('connection', function(socket){
-  socket.emit('nuevoUsr',shortid.generate()):
+  socket.emit('nuevoUsr',shortid.generate());
 });
 
 //Pantalla a proyectar
